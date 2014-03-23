43e891a8e64950bd4400b56096da755a4ec1d2c8
Курносов Виталий <>
2014-03-23 22:54:49 +0400

    code

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
  * @author me
  */
 
+var _ = require( 'underscore' );
+
+var obj = {
+    '123': true,
+    '121': true
+};
+
+var arr = _.map( obj, function( obj ) {
+    return obj;
+});
+
+console.log( arr );
